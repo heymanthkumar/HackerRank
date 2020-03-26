c250bf5992e0b283853ae1679d15343aca2d19df
Hemanth Tarumani <heymanth.net@gmail.com>
2020-03-26 19:59:20 -0400

    Code for the problem "A very big sum"

---
+++ ProblemSolving-Algorithms/AVeryBigSum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ProblemSolving_Algorithms
+{
+    //https://www.hackerrank.com/challenges/a-very-big-sum/problem
+    class AVeryBigSum
+    {
+        // Complete the aVeryBigSum function below.
+        static long aVeryBigSum(long[] ar)
+        {
+            long sum = 0;
+            foreach (long i in ar)
+                sum += i;
+
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+            int arCount = Convert.ToInt32(Console.ReadLine());
+
+            long[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt64(arTemp));
+            long result = aVeryBigSum(ar);
+
+            textWriter.WriteLine(result);
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
