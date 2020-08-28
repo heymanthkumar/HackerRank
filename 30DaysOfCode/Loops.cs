1dbb8d37cd8d27023517146b29bd5416b119a885
Hemanth Tarumani <heymanth.net@gmail.com>
2020-08-27 23:06:56 -0400

    Loops

---
+++ 30DaysOfCode/Loops.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30DaysOfCode
+{
+    class Loops
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if ((n >= 2) && n <= 20)
+            {
+                for (int i = 1; i < 11; i++)
+                    Console.WriteLine(n + " x " + i + " = " + n * i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
