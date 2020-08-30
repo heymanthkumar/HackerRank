0038f37ba394b476c3ef398a0c353759dfdec52f
Hemanth Tarumani <heymanth.net@gmail.com>
2020-08-29 21:46:05 -0400

    Arrays

---
+++ 30DaysOfCode/Arrays.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30DaysOfCode
+{
+    class Arrays
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] ar = Console.ReadLine().Split(' ');
+            for(int i=ar.Length; i>0; i--)
+            {
+                Console.Write(ar[i-1] + " ");
+            }
+        }
+    }
+}
