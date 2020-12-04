1c2c50bd8c719b8f2e407058c3843840fd84d4bb
Hemanth Tarumani <heymanth.net@gmail.com>
2020-12-04 12:14:53 -0500

    miniMaxSum

---
+++ ProblemSolving-Algorithms/MiniMaxSum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_Algorithms
+{
+    class MiniMaxSum
+    {
+        static void miniMaxSum(int[] arr)
+        {
+            int size = 4;
+            Array.Sort(arr);
+            int min = 0, max = 0;
+            for(int i=0; i<size; i++)            
+                min = min + arr[i];             
+
+            for (int i = arr.Length; i > (arr.Length - size); i--)            
+                max = max + arr[i-1];            
+
+            Console.WriteLine(min + " " + max);
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            miniMaxSum(arr);
+        }
+    }
+}
