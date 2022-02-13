ee89101c1d3359e81d16614bcd10c842fa7b97a6
Hemanth Tarumani <heymanthkumar@outlook.com>
2022-02-12 23:13:50 -0500

    TimeConversion.cs

---
+++ ProblemSolving-Algorithms/TimeConversion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ProblemSolving_Algorithms
+{
+    internal class TimeConversion
+    {
+        public static string timeConversion(string s)
+        {
+            return DateTime.Parse(s).ToString("HH:mm:ss");            
+
+        }
+        public static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string s = Console.ReadLine();
+
+            string result = timeConversion(s);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
