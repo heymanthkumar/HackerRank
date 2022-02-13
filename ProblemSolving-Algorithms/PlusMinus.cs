bc2b1c9621d3f6c59b02e8d23219d125f7bf3859
Hemanth Tarumani <heymanth.net@gmail.com>
2022-02-12 21:27:26 -0500

    PlusMinus

---
+++ ProblemSolving-Algorithms/PlusMinus.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_Algorithms
+{
+    internal class PlusMinus
+    {
+        // Complete the plusMinus function below.
+        static void plusMinus(int[] arr)
+        {
+            int countPositive = 0, countNegative = 0, countZero = 0;
+            int arrlength = arr.Length;
+            foreach (int i in arr)
+            {
+                if (i == 0)
+                    countZero++;
+                else if (i > 0)
+                    countPositive++;
+                else
+                    countNegative++;
+            }
+            Console.WriteLine((double)countPositive / arrlength);
+            Console.WriteLine((double)countNegative / arrlength);
+            Console.WriteLine((double)countZero / arrlength);
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+            ;
+            plusMinus(arr);
+        }
+    }
+}
