c430b5b179a7acf87af05c1c216e021aa4b07386
heymanthkumar <heymanthkumar@outlook.com>
2022-09-11 01:02:50 -0400

    AppleAndOrange solution

---
+++ ProblemSolving-Algorithms/AppleAndOrange.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_Algorithms
+{
+    //https://www.hackerrank.com/challenges/apple-and-orange/
+    internal class AppleAndOrange
+    {
+        public static void countApplesAndOranges(int s, int t, int a, int b, List<int> apples, List<int> oranges)
+        {
+            int appleCount = apples.Count(x => (x + a >= s && x + a <= t));
+            int orangeCount = oranges.Count(x => (x + b >= s && x + b <= t));
+            Console.WriteLine(appleCount);
+            Console.WriteLine(orangeCount);
+        }
+        public static void Main(string[] args)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int s = Convert.ToInt32(firstMultipleInput[0]);
+            
+            int t = Convert.ToInt32(firstMultipleInput[1]);
+
+            string[] secondMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int a = Convert.ToInt32(secondMultipleInput[0]);
+
+            int b = Convert.ToInt32(secondMultipleInput[1]);
+
+            string[] thirdMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int m = Convert.ToInt32(thirdMultipleInput[0]);
+
+            int n = Convert.ToInt32(thirdMultipleInput[1]);
+
+            List<int> apples = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(applesTemp => Convert.ToInt32(applesTemp)).ToList();
+
+            List<int> oranges = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(orangesTemp => Convert.ToInt32(orangesTemp)).ToList();
+
+            countApplesAndOranges(s, t, a, b, apples, oranges);
+        }
+    }
+}
